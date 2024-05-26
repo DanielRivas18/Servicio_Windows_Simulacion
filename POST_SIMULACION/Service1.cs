5e7d5dd40188a6fabf7f7fba33056b5a7692b005
DanielRivas18 <jose.rivas@uptc.edu.co>
2024-05-26 14:42:34 -0500

    CAMBIOS

--- POST_SIMULACION/Service1.cs
+++ POST_SIMULACION/Service1.cs
@@ -20,10 +20,8 @@ namespace POST_SIMULACION
         private Timer timer;
         private static readonly HttpClient client = new HttpClient();
         private Random random;
-        private const int minStudentId = 1;  
-        private const int maxStudentId = 1000;  
-        private const int minSubjectId = 1;  
-        private const int maxSubjectId = 50;  
+        private const int maxStudentId = 2000;
+        private const int maxSubjectId = 49;
 
         public Service1()
         {
@@ -33,8 +31,8 @@ namespace POST_SIMULACION
         protected override void OnStart(string[] args)
         {
             random = new Random();
-            int tiempo = Settings1.Default.TIEMPO;
-            timer = new Timer(1000); // Configurar el temporizador para 1 minuto
+           
+            timer = new Timer(10 * 10000); 
             timer.Elapsed += OnTimerElapsed;
             timer.Start();
         }
@@ -44,37 +42,24 @@ namespace POST_SIMULACION
             EnrollRandomStudent();
         }
 
-        private async void EnrollRandomStudent()
+        private async Task EnrollRandomStudent()
         {
-            var studentId = random.Next(minStudentId, maxStudentId + 1);
-            var subjectId = random.Next(minSubjectId, maxSubjectId + 1);
+            var studentId = GetRandomStudentId();
+            var subjectId = GetRandomSubjectId();
 
-            if (!await IsAlreadyEnrolled(studentId, subjectId))
-            {
-                await EnrollStudentAsync(studentId, subjectId);
-            }
+            await EnrollStudentAsync(studentId, subjectId);
         }
 
-        private async Task<bool> IsAlreadyEnrolled(int studentId, int subjectId)
+        private int GetRandomSubjectId()
         {
-            var enrollmentCheck = new
-            {
-                subject_id = subjectId,
-                student_id = studentId
-            };
+           
+            return random.Next(1, maxSubjectId+1); 
+        }
 
-            var json = JsonConvert.SerializeObject(enrollmentCheck);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync("http://localhost:3000/enrollments", content);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var result = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<CheckEnrollmentResponse>(result).IsEnrolled;
-            }
-
-            return false;
+        private int GetRandomStudentId()
+        {
+          
+            return random.Next(1, maxStudentId+1); 
         }
 
         private async Task EnrollStudentAsync(int studentId, int subjectId)
@@ -90,24 +75,12 @@ namespace POST_SIMULACION
 
             var response = await client.PostAsync("http://localhost:3000/enrollments", content);
 
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine($"Successfully enrolled student {studentId} in subject {subjectId}");
-            }
-            else
-            {
-                Console.WriteLine($"Failed to enroll student {studentId} in subject {subjectId}: {response.ReasonPhrase}");
-            }
+         
         }
 
         protected override void OnStop()
         {
             timer.Stop();
         }
-    }
-
-    public class CheckEnrollmentResponse
-    {
-        public bool IsEnrolled { get; set; }
     }
 }
